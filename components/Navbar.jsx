197a99f7d9b29cf592bd63c4640035cb26fc2b05
laith <laithalwani@gmail.com>
2024-07-09 12:49:12 -0400

    styles buttons and navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,11 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Navbar() {
   return (
     <nav>
+      <div className="logo">
+        <h3>Date Bliss</h3>
+      </div>
       <SignedOut>
-        <SignInButton />
+        <SignInButton className="btn" />
       </SignedOut>
       <SignedIn>
         <UserButton />
