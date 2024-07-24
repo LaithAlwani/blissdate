03607cba1a4b03cbb8c2ddeb0991b3ebbfb5f879
laith <laithalwani@gmail.com>
2024-07-24 13:01:48 -0400

    adds PWA

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Bliss Date",
   description: "The new way to find a partner!",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa","dating"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "Laith Alwani",
+      url: "https://www.linkedin.com/in/laith-alwani/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/dating-128.png" },
+    { rel: "icon", url: "/dating-128.png" },
+  ],
 };
 
 export default function RootLayout({ children }) {
