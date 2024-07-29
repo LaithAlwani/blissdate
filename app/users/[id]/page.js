e90e2a9d16b20a9b7f9cd32e110613da8659ed14
laith <laithalwani@gmail.com>
2024-07-29 12:46:45 -0400

    chnages profile layout and adds edit component

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,10 +1,17 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import "@/styles/profile.css";
+import { RiImageAddLine } from "react-icons/ri";
+import { MdVerified, MdClose  } from "react-icons/md";
+import { FaUserEdit } from "react-icons/fa";
 
 export default function UserPage() {
   const { user } = useUser();
+  const inputRef = useRef();
   const [currentUser, setCurrentUser] = useState();
+  const [toggleEditModle, setToggleEditModle] = useState(false);
 
   const getCurrentUser = async (userId) => {
     const res = await fetch(`/api/user/${userId}`);
@@ -21,19 +28,51 @@ export default function UserPage() {
 
   return (
     currentUser && (
-      <div>
-        <img src={currentUser.avatar} alt="" style={{ width: "64px" }} />
-        <p>
-          {currentUser.first_name} {currentUser.last_name}
-        </p>
-        <p>{currentUser.email}</p>
-        <span>{currentUser.clerk_id}</span>
-        <h3>images</h3>
-        {currentUser.images.map((img, i) => (
-          <img key={i} src={img} />
-        ))}
-        <button className="btn">Add more</button>
-      </div>
+      <section className="profile">
+        <button className="btn  btn-trasparent fixed right" onClick={() => setToggleEditModle(!toggleEditModle)}>
+          <FaUserEdit size={24} color="#f14a4f"/>
+        </button>
+        <img src={currentUser.avatar} alt="" className="avatar" />
+        <h3>
+          {currentUser.first_name} {currentUser.last_name}{" "}
+          {currentUser.verified && <MdVerified color="blue" />}
+        </h3>
+        <h4>{currentUser.email}</h4>
+        <div className="img-list">
+          {currentUser.images.map((img, i) => (
+            <div key={i} className="img-wrapper">
+              <img src={img} />
+            </div>
+          ))}
+          <input type="file" hidden ref={inputRef} />
+          <button className="btn btn-gray" onClick={() => inputRef.current.click()}>
+            <RiImageAddLine size={64} />
+          </button>
+        </div>
+        {toggleEditModle && <EditUser user={currentUser} setToggleEditModle={setToggleEditModle} />}
+      </section>
     )
   );
 }
+
+const EditUser = ({ user, setToggleEditModle }) => {
+  const { first_name, last_name, email, avatar } = user;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("submit")
+    setToggleEditModle(false);
+  }
+  return (
+    <section className="modle">
+      <button onClick={() => setToggleEditModle(false)} className="btn btn-trasparent fixed right">
+        <MdClose size={32} color="#f14a4f"/>
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={first_name} />
+        <input type="text" value={last_name} />
+        <input type="email" value={email} />
+        <button className="btn">Save</button>
+      </form>
+    </section>
+  );
+};
