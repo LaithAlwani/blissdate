d750ab72d77a1efc5062ce749c2bcdfecd1ebed1
laith <laithalwani@gmail.com>
2024-08-13 13:59:39 -0400

    adds dashboard fetch

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,20 @@
-export default function dashboardPage() {
+import User from "@/models/user";
+import connectToDB from "@/utils/database";
+
+export default async function dashboardPage() {
+  await connectToDB();
+  const users = await User.find();
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      {users?.map((user) => (
+        <div key={user._id}>
+          {console.log(user.verified)}
+          <h2>{user?.username}</h2>
+          <img src={user.images[0]} alt="" />
+          <p>{user.email}</p>
+          <p>verified: {user.verified? "Yes":"No"}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
